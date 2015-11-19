870f3c29324a5e30ba81fa7d2dc66e4f5eacfdcd
Brian Lauber <constructible.truth@gmail.com>
2015-11-19 10:46:41 -0500

    Starting 2.0 branch

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pintium',
-    version = '1.1.0',
+    version = '2.0.0a1',
     description = 'Pinterest + Selenium = Pintium',
     author = 'Brian Lauber',
     author_email = 'constructible.truth@gmail.com',
