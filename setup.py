69c09bdffcfd9d58d02e998ccfeceb0e4e558721
Brian Lauber <constructible.truth@gmail.com>
2015-12-04 10:34:42 -0500

    Bumped version to 2.0.0a2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pintium',
-    version = '2.0.0a1',
+    version = '2.0.0a2',
     description = 'Pinterest + Selenium = Pintium',
     author = 'Brian Lauber',
     author_email = 'constructible.truth@gmail.com',
